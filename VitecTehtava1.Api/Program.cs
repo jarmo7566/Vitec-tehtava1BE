a30ddd84281c80ca6b13ae75f54f95c86f9f3c05
JM <jarmo.makin@gmail.com>
2025-11-05 18:16:11 +0200

    db migration

--- VitecTehtava1.Api/Program.cs
+++ VitecTehtava1.Api/Program.cs
@@ -1,22 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using VitecTehtava1.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+string connectionString = builder.Configuration.GetConnectionString("Default") ?? 
+    throw new InvalidOperationException("Connection string 'Default' not found.");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
