d5022c8aba9f18e64478802d720fd1e7db6d5c19
JM <jarmo.makin@gmail.com>
2025-11-06 12:32:48 +0200

    ok

--- VitecTehtava1.Api/Program.cs
+++ VitecTehtava1.Api/Program.cs
@@ -6,9 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-string connectionString = builder.Configuration.GetConnectionString("Default") ?? 
-    throw new InvalidOperationException("Connection string 'Default' not found.");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+//Problems loading reference "https>json.schemastore.org... 
+//TODO: Fix later pirullinen vika, joka johtuu palvelun ylikuormituksesta
+//string connectionString = builder.Configuration.GetConnectionString("Default") ?? 
+//throw new InvalidOperationException("Connection string 'Default' not found.");
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql("Host=localhost;Port=5432;Database=UserDb;Username=postgres;Password=psLDV2"));
 
 var app = builder.Build();
 
